10549685d7d42e3d244bf9f1676edd05ba6a980f
voidPointer <mobyclub11062@gmail.com>
2026-03-30 21:16:52 +0300

    fix: use virt addr for multiboot_info access

--- src/kernel.c
+++ src/kernel.c
@@ -7,12 +7,14 @@
 #include "memory.h"
 #include "multiboot.h"
 
-void kernel_main(uint32_t magic, struct multiboot_info *bootInfo){
+void kernel_main(uint32_t magic, unsigned int physBootInfoAddr){
     term_init();
     init_GDT();
     init_IDT();
     init_PIT(10);
     init_Keyboard();
+
+    struct multiboot_info *bootInfo = (struct multiboot_info *)((physAddr) + KERNEL_START);
 
     uint32_t mod1 = *(uint32_t*)(bootInfo->mods_addr + 4);
     uint32_t psysicalAllocStart = (mod1 + 0xFFF) & ~0XFFF;
