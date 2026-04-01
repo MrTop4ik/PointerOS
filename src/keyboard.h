866dc87d2bd4073a8a0b3517357b635e2d8b7eff
voidPointer <mobyclub11062@gmail.com>
2026-04-01 21:37:12 +0300

    Translition to x86_64: 32-bit version archived in x86_32 branch

--- src/keyboard.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include "vga/vga.h"
-#include "utils/utils.h"
-#include "idt/idt.h"
-
-void irq1_handler(struct InterruptRegisters *regs);
-void init_Keyboard(void);
--- src/pit.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include "utils/utils.h"
-#include "idt/idt.h"
-#include "vga/vga.h"
-
-void irq0_handler(struct InterruptRegisters *regs);
-void init_PIT(uint32_t freq);
