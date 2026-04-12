bddd7b505f8678a5686192c25413c349af978169
voidPointer <mobyclub11062@gmail.com>
2026-04-12 19:41:41 +0300

    feat: add new structures to multiboot2.h

--- src/multiboot2.h
+++ src/multiboot2.h
@@ -3,6 +3,7 @@
 
 #define MULTIBOOT2_BOOTLOADER_MAGIC 0x36d76289
 
+#define MULTIBOOT_TAG_ALIGN                  8
 #define MULTIBOOT_TAG_TYPE_END               0
 #define MULTIBOOT_TAG_TYPE_CMDLINE           1
 #define MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME  2
@@ -22,6 +23,19 @@
 #define MULTIBOOT_TAG_TYPE_NETWORK           16
 #define MULTIBOOT_TAG_TYPE_EFI_MMAP          17
 #define MULTIBOOT_TAG_TYPE_EFI_BS            18
+#define MULTIBOOT_TAG_TYPE_EFI32_IH          19
+#define MULTIBOOT_TAG_TYPE_EFI64_IH          20
+#define MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR    21
+
+#define MULTIBOOT_MEMORY_AVAILABLE              1
+#define MULTIBOOT_MEMORY_RESERVED               2
+#define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE       3
+#define MULTIBOOT_MEMORY_NVS                    4
+#define MULTIBOOT_MEMORY_BADRAM                 5
+
+#define MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED      0
+#define MULTIBOOT_FRAMEBUFFER_TYPE_RGB          1
+#define MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT     2
 
 struct multiboot_tag {
     uint32_t type;
@@ -43,17 +57,42 @@ struct multiboot_tag_mmap {
     struct multiboot_mmap_entry entries[];
 } __attribute__((packed));
 
+struct multiboot_header_tag_framebuffer {
+    uint16_t type;
+    uint16_t flags;
+    uint32_t size;
+    uint32_t width;
+    uint32_t height;
+    uint32_t depth;
+}__attribute__((packed));
+
 struct multiboot_tag_framebuffer {
     uint32_t type;
     uint32_t size;
-    uint64_t common_addr;
-    uint32_t common_pitch;
-    uint32_t common_width;
-    uint32_t common_height;
-    uint8_t  common_bpp;
-    uint8_t  common_type;
-    uint8_t  reserved;
-} __attribute__((packed));
+
+    uint64_t framebuffer_addr;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t framebuffer_bpp;
+    uint8_t framebuffer_type;
+    uint16_t reserved;
+
+    union {
+        struct {
+            uint16_t framebuffer_palette_num_colors;
+        };
+
+        struct {
+            uint8_t framebuffer_red_field_position;
+            uint8_t framebuffer_red_mask_size;
+            uint8_t framebuffer_green_field_position;
+            uint8_t framebuffer_green_mask_size;
+            uint8_t framebuffer_blue_field_position;
+            uint8_t framebuffer_blue_mask_size;
+        };
+    };
+}__attribute__((packed));
 
 struct multiboot_info {
     uint32_t total_size;
