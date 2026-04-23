2f1aadafbaadaffa970574622a1772c85bd624b1
voidPointer <mobyclub11062@gmail.com>
2026-04-23 21:49:06 +0300

    refactor: replace magic numbers with constants

--- src/memory/vmm.c
+++ src/memory/vmm.c
@@ -1,7 +1,16 @@
 #include "vmm.h"
 
-#define KERNEL_OFFSET   0xFFFFFFFF80000000
+#define KERNEL_OFFSET 0xFFFFFFFF80000000
 #define DIRECT_OFFSET 0xFFFF800000000000
+
+#define PAGE_SIZE_4KB 4096
+#define PAGE_MASK_4KB (~(PAGE_MASK_4KB - 1))
+#define PAGE_MASK_2MB 0x200000
+
+#define PTE_PRESENT   (1ULL << 0)
+#define PTE_WRITABLE  (1ULL << 1)
+#define PTE_USER      (1ULL << 2)
+#define PET_PAGE_SIZE (1ULL << 7)
 
 void init_VMM(unsigned int bootInfoAddr){
     struct multiboot_info* virtBootInfo = (struct multiboot_info *)(bootInfoAddr + KERNEL_OFFSET);
@@ -34,32 +43,32 @@ void vmm_init_direct_mapping(uint64_t maxAddr){
     uint64_t* old_pml4 = (uint64_t *)(read_cr3() + KERNEL_OFFSET);
     pml4[511] = old_pml4[511];
 
-    for (uint64_t paddr = 0; paddr < maxAddr; paddr += 0x200000){
+    for (uint64_t paddr = 0; paddr < maxAddr; paddr += PAGE_MASK_2MB){
         uint64_t vaddr = paddr + DIRECT_OFFSET;
 
         uint64_t pml4_indx = (vaddr >> 39) & 0x1FF;
         uint64_t pdpt_indx = (vaddr >> 30) & 0x1FF;
         uint64_t pd_indx = (vaddr >> 21) & 0x1FF;
 
-        if (!(pml4[pml4_indx] & 1)){
+        if (!(pml4[pml4_indx] & PTE_PRESENT)){
             uint64_t new_table_phys = pmm_alloc_page();
             uint64_t *new_table = (uint64_t *)(new_table_phys + KERNEL_OFFSET);
             memset(new_table, 0, 0x1000);
-            pml4[pml4_indx] = new_table_phys | 0x3;
+            pml4[pml4_indx] = new_table_phys | (PTE_PRESENT | PTE_WRITABLE);
         }
 
         uint64_t *pdpt = (uint64_t *)((pml4[pml4_indx] & ~0xFFF) + KERNEL_OFFSET);
 
-        if (!(pdpt[pdpt_indx] & 1)){
+        if (!(pdpt[pdpt_indx] & PTE_PRESENT)){
             uint64_t new_table_phys = pmm_alloc_page();
             uint64_t *new_table = (uint64_t *)(new_table_phys + KERNEL_OFFSET);
             memset(new_table, 0, 0x1000);
-            pdpt[pdpt_indx] = new_table_phys | 0x3;
+            pdpt[pdpt_indx] = new_table_phys | (PTE_PRESENT | PTE_WRITABLE);
         }
 
         uint64_t *pd = (uint64_t *)((pdpt[pdpt_indx] & ~0xFFF) + KERNEL_OFFSET);
 
-        pd[pd_indx] = paddr | 0x83;
+        pd[pd_indx] = paddr | (PTE_PRESENT | PTE_WRITABLE | PET_PAGE_SIZE);
     }
 
     write_cr3(pml4_phys);
