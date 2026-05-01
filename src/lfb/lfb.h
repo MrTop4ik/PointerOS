ceee886453efe011c55cc4233b70cfe7cd2ae7cc
voidPointer <mobyclub11062@gmail.com>
2026-05-01 12:39:47 +0300

    feat(lfb): add font8x16

--- src/lfb/lfb.h
+++ src/lfb/lfb.h
@@ -3,6 +3,8 @@
 #include "../multiboot2.h"
 #include "../memory/vmm.h"
 #include "../utils/inlineasm.h"
+#include "../serial/serial.h"
+#include "font8x16.h"
 
 #define LFB_ADDR 0xFFFFFFFFC0000000
 
